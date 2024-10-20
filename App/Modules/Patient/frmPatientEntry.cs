db02cba6be28bdcedcb8b51734b1776152eaee60
patarci <patcjanvier@gmail.com>
2024-10-21 02:55:51 +0800

    feature/HC9 - UI Design Patient Entry Form

---
+++ App/Modules/Patient/frmPatientEntry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App.Modules.Patient
+{
+    public partial class frmPatientEntry : Form
+    {
+        public frmPatientEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
